5e5062823bdcf7263d58feccb3cd2db46f295b7c
princerajkr45 <priyance5204@gmail.com>
2024-07-23 15:08:30 +0530

    get cuurent user

--- src/models/User.models.js
+++ src/models/User.models.js
@@ -67,7 +67,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 userSchema.methods.generateAccessToken = function () {
-     jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             username: this.username,
@@ -81,7 +81,7 @@ userSchema.methods.generateAccessToken = function () {
     )
 }
 userSchema.methods.generateRefreshToken = function () {
-     jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
         },
